341a2fea49304ad097d6af7107a84b4ab0429d87
Vinícius Santos <viniciusfs.santos@outlook.com>
2022-08-22 23:54:38 -0300

    checkpoint1

---
+++ index.js
@@ -0,0 +1,49 @@
+let tempoPipoca = 10,
+  tempoMacarrao = 8,
+  tempoCarne = 15,
+  tempoFeijao = 12,
+  tempoBrigadeiro = 8,
+  pipoca = "pipoca",
+  macarrao = "macarrao",
+  carne = "carne",
+  feijao = "feijao",
+  brigadeiro = "brigadeiro";
+
+function aquecer(prato, tempo) {
+  if ("pipoca" && tempo <= tempoPipoca) {
+    console.log("tempo insuficiente");
+  } else if ("pipoca" && tempo <= tempoPipoca * 2 - 1) {
+    console.log("Prato concluido com sucesso");
+  } else if ("pipoca" && tempo > tempoPipoca * 2 && tempo <= 29) {
+    console.log("Comida queimou");
+  } else if (pipoca && tempo >= tempoPipoca * 3) {
+    console.log("Kabumm");
+  } else if (macarrao && tempo <= tempoMacarrao * 2 - 1) {
+    console.log("Prato concluido com sucesso");
+  } else if (macarrao && tempo > tempoMacarrao * 2 && tempo <= 23) {
+    console.log("Comida queimou");
+  } else if (macarrao && tempo >= tempoMacarrao * 3) {
+    console.log("Kabumm");
+  } else if (carne && tempo < tempoCarne) {
+    console.log("tempo insuficiente");
+  } else if (carne && tempo <= tempoCarne * 2 - 1) {
+    console.log("Prato concluido com sucesso");
+  } else if (carne && tempo > tempoCarne * 2 && tempo <= 44) {
+    console.log("Comida queimou");
+  } else if (carne && tempo >= tempoCarne * 3) {
+    console.log("Kabumm");
+  } else if (feijao && tempo <= tempoFeijao * 2 - 1) {
+    console.log("Prato concluido com sucesso");
+  } else if (feijao && tempo > tempoFeijao * 2 && tempo <= 35) {
+    console.log("Comida queimou");
+  } else if (feijao && tempo >= tempoFeijao * 3) {
+    console.log("Kabumm");
+  } else if (brigadeiro && tempo <= tempoBrigadeiro * 2 - 1) {
+    console.log("Prato concluido com sucesso");
+  } else if (brigadeiro && tempo > tempoBrigadeiro * 2 && tempo <= 35) {
+    console.log("Comida queimou");
+  } else if (brigadeiro && tempo >= tempoBrigadeiro * 3) {
+    console.log("Kabumm");
+  } else console.log("Prato inexistente");
+}
+aquecer(brigadeiro, 24);
